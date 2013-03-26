69457f46e5e667eeccacd68f2dce37cc76c26abb
Martin Stutterheim <stutterheim@vicom-agency.de>
2013-03-26 22:22:07 +0100

    initial commit

---
+++ sites/all/libraries/ckeditor/plugins/styles/styles/default.js
@@ -0,0 +1,7 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.stylesSet.add('default',[{name:'Marker: Yellow',element:'span',styles:{'background-color':'Yellow'}},{name:'Computer Code',element:'code'},{name:'Image on Left',element:'img',attributes:{style:'padding: 5px 0; margin-right: 10px',border:'0',align:'left'}},{name:'Image on Right',element:'img',attributes:{style:'padding: 5px 0; margin-left: 10px',border:'0',align:'right'}},{name:'Borderless Table',element:'table',styles:{'border-style':'hidden','background-color':'#cccccc'}},{name:'Square Bulleted List',element:'ul',styles:{'list-style-type':'square'}}, {name:'Filesize',element:'a',attributes:{'class':'size'}}, {name:'Fileicon',element:'a',attributes:{'class':'icon'}}, {name:'File (icon + size)',element:'a',attributes:{'class':'icon size'}}, {name:'Imagecaption',element:'img',attributes:{'class':'cap'}}]);
+

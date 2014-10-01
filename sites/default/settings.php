4508bb049dedf39f71c5687b5f8605af72945032
Martin Stutterheim <stutterheim@vicom-agency.de>
2014-10-01 15:14:58 +0200

    modules/db_maintenance/

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -222,15 +222,15 @@ $databases = array (
   ),
 );
     break;
-case 'cph.vicom-agency.de':
+case 'cph2.vicom-agency.de':
 $databases = array (
   'default' => 
   array (
     'default' => 
     array (
-      'database' => 'db271743_24',
-      'username' => 'db271743_24',
-      'password' => 'RJLNueTpgg',
+      'database' => 'db271743_4',
+      'username' => 'db271743_4',
+      'password' => 'TB613F3mP7',
       'host' => 'mysql5.just-in-place.com',
       'port' => '',
       'driver' => 'mysql',

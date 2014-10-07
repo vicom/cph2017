583b29d2341a75e3e9bfdfc1b33afc43cad873cf
Martin Stutterheim <stutterheim@vicom-agency.de>
2014-10-07 10:20:50 +0200

    Change fron Bootstrap 2 to 3

--- sites/all/themes/bootstrap_subtheme_cph/template.php
+++
@@ -1,8 +0,0 @@
-<?php
-function bootstrap_subtheme_cph_preprocess_html(&$vars) {
-  foreach($vars['user']->roles as $role){
-    $vars['classes_array'][] = 'type-' . drupal_html_class($role);
-  }
-}
-
-?>

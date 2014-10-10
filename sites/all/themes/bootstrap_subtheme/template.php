2668338ce6ec9bf941b21580dabecda570437958
Martin Stutterheim <stutterheim@vicom-agency.de>
2014-10-10 15:56:34 +0200

    Style update

--- sites/all/themes/bootstrap_subtheme/template.php
+++ sites/all/themes/bootstrap_subtheme/template.php
@@ -4,3 +4,11 @@
  * @file
  * template.php
  */
+
+function bootstrap_subtheme_preprocess_html(&$vars) {
+  foreach($vars['user']->roles as $role){
+    $vars['classes_array'][] = 'type-' . drupal_html_class($role);
+  }
+}
+
+?>
